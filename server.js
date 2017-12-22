2ba05918621a51b7d2090812cf7be5e5a6c3d41e
zheng.shoujian <979923059@qq.com>
2017-12-22 22:06:34 +0800

    Access-Control-Allow-Origin

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Origin', '//www.zhengsj.top');
 	next();
 });
 
