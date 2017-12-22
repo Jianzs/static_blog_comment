aeb3492ec311769a8b33d0b1d29da42f4f760729
zhengSJ.aliyun <979923059@qq.com>
2017-12-22 22:10:10 +0800

    change access

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '//www.zhengsj.top');
+	res.setHeader('Access-Control-Allow-Origin', 'http://www.zhengsj.top');
 	next();
 });
 
